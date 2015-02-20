f37046b29f849115f920602e09f0ddf4889411d3
Pepijn de Vos <pepijndevos@gmail.com>
2015-02-20 12:22:53 +0100

    intitial RAW HID code

---
+++ tcpoke_teensy/tcpoke_teensy.ino
@@ -0,0 +1,44 @@
+#include <SPI.h>
+
+#define SERIAL_NO_DATA_BYTE 0xFE
+#define ESTABLISH_CONNECTION_WITH_INTERNAL_CLOCK 0x01
+#define ESTABLISH_CONNECTION_WITH_EXTERNAL_CLOCK 0x02
+#define SERIAL_NYBLE 0x60
+
+#define DELAY 100
+
+byte buffer[64];
+
+void setup() {
+    Serial.begin(115200);
+    Serial.print("hello world\n");
+
+    SPI.begin();
+    SPI.beginTransaction(SPISettings(8000, MSBFIRST, SPI_MODE3));
+}
+
+bool connected = false;
+
+void loop() {
+    byte in_data = 0;
+    byte out_data = 0;
+    if(connected) {
+        if(RawHID.recv(buffer, DELAY)) {
+            out_data = buffer[0];
+        } else {
+            out_data = SERIAL_NO_DATA_BYTE;
+        }
+    } else {
+        out_data = ESTABLISH_CONNECTION_WITH_INTERNAL_CLOCK;
+        delay(DELAY);
+    }
+    in_data = SPI.transfer(out_data);
+    Serial.print(in_data, HEX);
+    Serial.print("\n");
+
+    if(connected || in_data == SERIAL_NYBLE) {
+        connected = true;
+        buffer[0] = in_data;
+        RawHID.send(buffer, DELAY);
+    }
+}
