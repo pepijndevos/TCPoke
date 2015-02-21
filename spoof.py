33766ba0672f1d2d79045e275c0cadd5072c080a
Pepijn de Vos <pepijndevos@gmail.com>
2015-02-21 21:07:15 +0100

    tcp test

--- spoof.py
+++ spoof.py
@@ -1,16 +1,28 @@
-import hid, time
+import hid, time, sys, socket
 
 paths = [t['path'] for t in hid.enumerate(5824, 1158) if t['usage'] == 512]
 teensy = hid.device()
 teensy.open_path(paths[0])
 teensy.set_nonblocking(1)
+buf = bytearray([0]*64)
 
-if __name__ == "__main__":
-        while True:
-            try:
-                buf = teensy.read(64)
-                print(hex(buf[0]))
-                teensy.write(buf)
-            except IndexError:
-                pass
+with socket.create_connection((sys.argv[1], sys.argv[2])) as sock:
+    sock.setblocking(False)
+    while True:
+        try:
+            data_in = teensy.read(64)[0]
+        except IndexError:
+            pass
+        else:
+            print(">>", hex(data_in))
+            sock.send(bytearray([data_in]))
+
+        try:
+            data_out = ord(sock.recv(1))
+        except BlockingIOError:
+            pass
+        else:
+            print("<<", hex(data_out))
+            buf[0] = data_out
+            teensy.write(buf)
                 
