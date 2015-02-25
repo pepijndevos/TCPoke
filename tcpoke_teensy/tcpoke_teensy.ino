2db43e58af7fd79533edc5aff59cfc96f5853f5b
Pepijn de Vos <pepijndevos@gmail.com>
2015-02-25 09:18:02 +0100

    works on Teensy 2

--- tcpoke_teensy/tcpoke_teensy.ino
+++ tcpoke_teensy/tcpoke_teensy.ino
@@ -5,7 +5,7 @@
 #define ESTABLISH_CONNECTION_WITH_EXTERNAL_CLOCK 0x02
 #define SERIAL_NYBLE 0x60
 
-#define DELAY 500
+#define DELAY 1000
 
 byte buffer[64];
 
@@ -22,21 +22,19 @@ bool connected = false;
 void loop() {
     byte in_data = 0;
     byte out_data = RawHID.recv(buffer, DELAY);
-    if(connected) {
-        if(out_data) {
-            out_data = buffer[0];
-            if(buffer[1]) connected = false; // reset
-        } else {
-            out_data = SERIAL_NO_DATA_BYTE;
-        }
-    } else {
+    if(out_data) {
+        out_data = buffer[0];
+        if(buffer[1]) connected = false; // reset
+    } else if (!connected) {
         out_data = ESTABLISH_CONNECTION_WITH_INTERNAL_CLOCK;
+    } else {
+        return;
     }
     in_data = SPI.transfer(out_data);
     Serial.print(in_data, HEX);
     Serial.print("\n");
 
-    if((connected || in_data == SERIAL_NYBLE) && in_data != SERIAL_NO_DATA_BYTE) {
+    if(connected || in_data == SERIAL_NYBLE) {
         connected = true;
         buffer[0] = in_data;
         RawHID.send(buffer, DELAY);
